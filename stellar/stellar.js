7c8e5e5ded05f7fb3a25cceaf194cc1efbe220fd
minhdao <minhdao6@gmail.com>
2018-02-05 16:59:26 +0700

    add function to generate and add keys

--- stellar/stellar.js
+++ stellar/stellar.js
@@ -1,3 +1,21 @@
 const StellarBase = require('stellar-base');
-var keyPair = StellarBase.Keypair.random();
-var accountId = keyPair.publicKey();
+
+var genKey = () => {
+    return StellarBase.Keypair.random();
+};
+
+var getPublic = (keyPair) => {
+    return keyPair.publicKey();
+};
+
+var getPrivate = (keyPair) => {
+    return keyPair.secret();
+};
+
+var keyPair = genKey();
+
+module.exports = {
+    genKey,
+    getPublic,
+    getPrivate
+};
