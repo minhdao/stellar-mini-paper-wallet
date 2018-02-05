62e1bfaa05124a963202f2c53b15f9f60247c88b
minhdao <minhdao6@gmail.com>
2018-02-05 14:01:25 +0700

    clean up

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const StellarBase = require('stellar-base');
-const QR = require('qrcode');
 
 var keyPair = StellarBase.Keypair.random();
 var accountId = keyPair.publicKey();
 
 console.log(keyPair.publicKey());
 console.log(keyPair.secret());
-
-QR.toCanvas(keyPair.publicKey(), { errorCorrectionLevel: 'H' }, function (err, canvas) {
-  if (err) throw err;
-
-  var container = document.getElementById('canvas');
-  container.appendChild(canvas);
-});
