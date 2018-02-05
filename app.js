ba50296eb63c973ff2e2b77fc46e702ea217fe81
minhdao <minhdao6@gmail.com>
2018-02-05 17:31:30 +0700

    gen, get public and private key for user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const stellar = require('./stellar/stellar.js');
+
 var app = express();
 
 // set view engine
@@ -11,9 +13,12 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 // route config
 app.get('/', (req, res) => {
+    var keyPair = stellar.genKey();
     res.render('home.hbs', {
         pageName: 'welcome home',
         content: 'home sweet home',
+        publicKey: stellar.getPublic(keyPair),
+        privateKey: stellar.getPrivate(keyPair)
     });
 });
 
